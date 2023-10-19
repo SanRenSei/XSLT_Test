ad30927da18858789eeed25b974dfd1baae6d81f
Brendan <brendan928@gmail.com>
2023-10-19 19:59:20 -0400

    Fixed it

--- xsltTransformer/src/App.jsx
+++ xsltTransformer/src/App.jsx
@@ -11,21 +11,21 @@ let loadFile = async (fileName) => {
   return resBody;
 }
 
-let xslFileList = ['Abbr_Codes.xsl', 'iso_State_Codes.xsl', 'N-PORT_fundInfo.xsl', 'N-PORT_generalInfo.xsl', 'N-PORT_schPortfolio.xsl', 'NPORT_Documents.XSL', 'NPORT_explanatoryNotes.xsl', 'NPORT_signature.xsl', 'NPORT-P_X01.xsl', 'util.xsl'];
+//let xslFileList = ['Abbr_Codes.xsl', 'iso_State_Codes.xsl', 'N-PORT_fundInfo.xsl', 'N-PORT_generalInfo.xsl', 'N-PORT_schPortfolio.xsl', 'NPORT_Documents.XSL', 'NPORT_explanatoryNotes.xsl', 'NPORT_signature.xsl', 'NPORT-P_X01.xsl'];
+let xslFileList = ['NPORT-P_X01.xsl'];
 let xmlFileList = ['0000869392-23-000164.xml', '0000869392-23-002292.xml'];
 
 let loadApp = async () => {
   let xmlContent = await loadFile(xmlFileList[0]);
-  const xml = new DOMParser().parseFromString(xmlContent, 'text/xml');
-  const xsltProcessor = new XSLTProcessor();
+  let xml = new DOMParser().parseFromString(xmlContent, 'text/xml');
   for (let i=0;i<xslFileList.length;i++) {
+    const xsltProcessor = new XSLTProcessor();
     let xslContent = await loadFile(xslFileList[i]);
     const xslt = new DOMParser().parseFromString(xslContent, 'text/xml');
     xsltProcessor.importStylesheet(xslt);
+    xml = xsltProcessor.transformToDocument(xml);
   }
-  let resultDocument = xsltProcessor.transformToFragment(xml, document);
-  document.getElementById("appContainer").appendChild(resultDocument);
-  console.log(resultDocument);
+  document.getElementById("appContainer").appendChild(xml.documentElement);
 }
 
 function App() {
